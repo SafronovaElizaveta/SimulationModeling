02fbe456ec98524fada2cf10df64dec807a052bc
SafronovaElizaveta <liza20010107@mail.ru>
2021-06-06 16:40:44 +0700

    add_lab18

---
+++ lab18/Lab18/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Lab18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Моделирование";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "n1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 107);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "n2";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(49, 73);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(63, 22);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Location = new System.Drawing.Point(49, 105);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(63, 22);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 139);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "λ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 172);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "μ";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 2;
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown3.Location = new System.Drawing.Point(49, 137);
+            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(63, 22);
+            this.numericUpDown3.TabIndex = 7;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            314,
+            0,
+            0,
+            131072});
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.DecimalPlaces = 2;
+            this.numericUpDown4.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown4.Location = new System.Drawing.Point(49, 170);
+            this.numericUpDown4.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(63, 22);
+            this.numericUpDown4.TabIndex = 8;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Location = new System.Drawing.Point(124, 119);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            series3.ChartArea = "ChartArea1";
+            series3.MarkerColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(442, 369);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            this.chart2.Location = new System.Drawing.Point(608, 119);
+            this.chart2.Margin = new System.Windows.Forms.Padding(4);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(458, 369);
+            this.chart2.TabIndex = 10;
+            this.chart2.Text = "chart2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(339, 78);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(509, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Сравнение эмпирического стационарного распределения с теоретическим";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(446, 28);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(158, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Количество операций:";
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(608, 26);
+            this.numericUpDown5.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(63, 22);
+            this.numericUpDown5.TabIndex = 13;
+            this.numericUpDown5.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 17);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 28);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1104, 535);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ lab18/Lab18/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab18
+{
+
+    public partial class Form1 : Form
+    {
+        Model md;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            md = new Model((double)numericUpDown3.Value, (double)numericUpDown4.Value, (int)numericUpDown1.Value, (double)numericUpDown2.Value, (int)numericUpDown5.Value);
+
+
+            md.Probability(md.OriginalAO(), md.OriginalAQP());
+            md.Statistics();
+
+            var sortedStat = new SortedDictionary<int, double>(md.stat);
+
+            foreach (int i in sortedStat.Keys)
+            {
+                chart2.Series[0].Points.AddXY(i, md.stat[i]);
+            }
+
+            foreach (int i in md.freq.Keys)
+            {
+                chart1.Series[0].Points.AddXY(i, md.freq[i]);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart2.Series[0].Points.Clear();
+        }
+    }
+
+    public class Model
+    {
+        double next_person, next_operator;
+        double time = 0;
+        double T;
+        int k;
+        int N;
+
+        double lambda;
+        double psi;
+        double rho;
+
+        int number_operator;
+        double ISD;
+
+        public Dictionary<int, double> freq = new Dictionary<int, double>();
+        public Dictionary<int, double> stat;
+
+        public Model(double l, double m, int n0, double t0, int noo)
+        {
+            lambda = l;
+            psi = m;
+            rho = l / m;
+            N = n0;
+            T = t0;
+            k = 0;
+            number_operator = noo;
+            ISD = OriginalStationaryDistribution(noo);
+
+
+        }
+        public Agent OriginalAQP()
+        {
+            Agent AQP = new Agent();
+            AQP.CreateAgentQueuePerson(lambda);
+            return AQP;
+        }
+        public Agent OriginalAO()
+        {
+            Agent AO = new Agent();
+            AO.CreateAgentOperator(psi, number_operator);
+            return AO;
+        }
+
+        public void Statistics()
+        {
+            stat = freq;
+
+            foreach (int i in stat.Keys.ToList())
+            {
+                if (i < number_operator)
+                {
+                    stat[i] = (Math.Pow(rho, i) / Factorial(i)) * ISD;
+                }
+                else
+                {
+                    stat[i] = (Math.Pow(rho, i) / (Factorial(i) * Math.Pow(number_operator, i - number_operator))) * ISD;
+                }
+            }
+        }
+
+        public void Probability(Agent ao, Agent aqp)
+        {
+            while (k < N)
+            {
+                while (time < T)
+                {
+                    next_operator = ao.GetNextEventOperation();
+                    next_person = aqp.GetNextEventForQueueClient();
+                    if (next_person < next_operator)
+                    {
+                        aqp.ProcessEventForQueueClient(ao);
+                        time += next_person;
+                    }
+                    else
+                    {
+                        ao.ProcessEventOperation(aqp);
+                        time += next_operator;
+                    }
+                }
+                k++;
+                time = 0;
+                try
+                {
+                    freq.Add(ao.engaged_orerator_ + aqp.number_client_, 0);
+                }
+                catch
+                {
+
+                }
+
+                freq[ao.engaged_orerator_ + aqp.number_client_]++;
+
+            }
+            foreach (int i in freq.Keys.ToList())
+            {
+                freq[i] /= N;
+            }
+        }
+
+
+        private double OriginalStationaryDistribution(int noo)//ISD
+        {
+            double temp = 0;
+
+            for (int i = 0; i < noo; i++)
+            {
+                temp += Math.Pow(rho, i) / Factorial(i);
+            }
+
+            double temp1 = Math.Pow(rho, (noo + 1)) / Factorial(noo) * (noo - rho);
+
+            return Math.Pow((temp + temp1), -1);
+        }
+        private int Factorial(int i)
+        {
+            int temp = 1;
+
+            for (int j = 1; j <= i; j++)
+            {
+                temp *= j;
+            }
+
+            return temp;
+        }
+    }
+
+    public class Agent
+    {
+        double lambda;
+        double psi;
+        
+        int engaged_orerator;
+        int number_operator;
+        int number_client;
+        
+        Random rnd = new Random();
+
+        public void CreateAgentOperator(double m, int noo)
+        {
+            psi = m;
+            number_operator = noo;
+        }
+
+        public void CreateAgentQueuePerson(double l)
+        {
+            lambda = l;
+            number_client = 0;
+        }
+
+        public int engaged_orerator_ { get => engaged_orerator; set => engaged_orerator = value; }
+        public int number_operator_ { get => number_operator; }
+        public int number_client_ { get => number_client; set => number_client = value; }
+
+        public double GetNextEventOperation()
+        {
+            if (engaged_orerator > 0)
+            {
+                double A = rnd.NextDouble();
+                double temp = A * engaged_orerator;
+                return (-Math.Log(temp) / psi);
+            }
+            else { return Double.PositiveInfinity; }
+        }
+
+        public void ProcessEventOperation(Agent aqp)
+        {
+            if (aqp.number_client_ == 0)
+            {
+                engaged_orerator--;
+            }
+            else
+            {
+                aqp.number_client_--;
+            }
+        }
+
+        public double GetNextEventForQueueClient()
+        {
+            double A = rnd.NextDouble();
+            return (-Math.Log(A) / lambda);
+        }
+
+        public void ProcessEventForQueueClient(Agent ao)
+        {
+            if (ao.engaged_orerator_ < ao.number_operator_)
+            {
+                ao.engaged_orerator_++;
+            }
+            else
+            {
+                number_client++;
+            }
+        }
+    }
+}
